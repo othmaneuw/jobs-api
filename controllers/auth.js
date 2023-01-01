dd29120d348ca229075a524b402f69dcf498ac44
othmaneuw <strangeothmane@gmail.com>
2023-01-01 18:26:05 +0100

    User model created && hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
+const User = require('../models/User');
+const {StatusCodes} = require('http-status-codes');
+const bcrypt = require('bcryptjs');
 
+const register = async (req,res) =>{
 
-const register = (req,res) =>{
-    res.send('register request');
+    const {name,email,password} = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    const tempUser = {name,email,password:hashedPassword};
+
+    const user = await User.create({...tempUser});
+    res.status(StatusCodes.CREATED).json({user});
 }
 
 const login = (req,res) =>{
