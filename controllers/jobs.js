9cf17323259ce7091a3bc1327e05818ff7ccb0df
othmaneuw <strangeothmane@gmail.com>
2023-01-02 23:20:20 +0100

    get single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,31 +1,43 @@
-const Job = require('../models/Job');
-const {StatusCodes} = require('http-status-codes');
-const {BadRequestError,NotFoundError} = require('../errors');
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
 
-const getAllJobs = async (req,res) =>{
-    const jobs = await Job.find({createdBy : req.user.id});
-    res.status(StatusCodes.OK).json({jobs,count : jobs.length});
-}
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.id });
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
 
-const getJob = (req,res) =>{
-    res.send('Get job');
-}
-const createJob = async (req,res) =>{
-    req.body.createdBy = req.user.id;
-    const job = await Job.create(req.body);
-    res.status(StatusCodes.CREATED).json({job});
-}
-const updateJob = (req,res) =>{
-    res.send('Update job');
-}
-const deleteJob = (req,res) =>{
-    res.send('Delete job');
-}
+const getJob = async (req, res) => {
+  const {
+    user: { id: userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with the id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
+};
+
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.id;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+const updateJob = (req, res) => {
+  res.send("Update job");
+};
+const deleteJob = (req, res) => {
+  res.send("Delete job");
+};
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    deleteJob,
-    updateJob
+  getAllJobs,
+  getJob,
+  createJob,
+  deleteJob,
+  updateJob,
 };
