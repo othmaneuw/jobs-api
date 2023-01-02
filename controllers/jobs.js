e20cc36b5baf41ac1176583773f6f2afa71ec86b
othmaneuw <strangeothmane@gmail.com>
2023-01-02 22:41:00 +0100

    create and get all jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,13 +1,19 @@
+const Job = require('../models/Job');
+const {StatusCodes} = require('http-status-codes');
+const {BadRequestError,NotFoundError} = require('../errors');
 
-const getAllJobs = (req,res) =>{
-    res.send('Get all jobs');
+const getAllJobs = async (req,res) =>{
+    const jobs = await Job.find({createdBy : req.user.id});
+    res.status(StatusCodes.OK).json({jobs,count : jobs.length});
 }
 
 const getJob = (req,res) =>{
     res.send('Get job');
 }
-const createJob = (req,res) =>{
-    res.json({...req.user});
+const createJob = async (req,res) =>{
+    req.body.createdBy = req.user.id;
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({job});
 }
 const updateJob = (req,res) =>{
     res.send('Update job');
