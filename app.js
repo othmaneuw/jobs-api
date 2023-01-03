4e110b78a3fb1219a57ee8b0d43eb618257a3da3
othmaneuw <strangeothmane@gmail.com>
2023-01-03 13:08:58 +0100

    extra security packages

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+
+
 const express = require('express');
 const app = express();
 
@@ -14,7 +22,16 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})) 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
